f1468600fbe03f74aa40b587d6e21d3d8280440a
david <dpvanbalen@hotmail.com>
2025-06-09 23:15:32 +0200

    getting somewhere!

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,7 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (placeholder, value, style, src)
+import Html.Attributes exposing (placeholder, value, style, src, type_)
 import Html.Events exposing (onClick)
 import Task
 import Time
@@ -82,23 +82,27 @@ view model =
     Nothing -> div [] [button [onClick StartGame] [text "Begin!"]]
     Just endtime ->
       let millisdiff = Time.posixToMillis endtime - Time.posixToMillis model.time in 
-      div [] (rows 
-        [ (90, cols
-          [ (40, [Svg.svg [ Svg.width "300", Svg.height "400", Svg.viewBox "0 0 300 400" ] -- een witte rechthoek. puur omdat 
-            [ Svg.rect [ Svg.x "0", Svg.y "0", Svg.width "300", Svg.height "400", Svg.fill "#ffffff" ] []
-            , Svg.foreignObject [Svg.x "0", Svg.y "0", Svg.width "300", Svg.height "400"] (rows 
-            [ (60,  [text "vraag hier"])
-            , (20,  [input [placeholder "antwoord", value model.answer] [], button [] [text "indienen"]])
-            , (20, [button [] [text "Vorige vraag"], button [] [text "Volgende vraag"]]) 
+      div [style "background-image" "url('images/astrid.jpg')", style "background-size" "100%", style "height" "100%"] (rows 
+        [ (85, [], cols
+          [ (25, [Svg.svg [ Svg.width "100%", Svg.height "100%", Svg.viewBox "0 0 100% 100%" ] -- een witte rechthoek. puur omdat 
+            [ Svg.rect [ Svg.x "0", Svg.y "0", Svg.width "100%", Svg.height "100%", Svg.fill "#ffffff" ] []
+            , Svg.foreignObject [Svg.x "0", Svg.y "0", Svg.width "300", Svg.height "100%"] (rows 
+            [ (60, [], [text "vraag hier"])
+            , (20, [], [input [placeholder "antwoord", value model.answer] [], button [] [text "indienen"]])
+            , (20, [], [button [] [text "Vorige vraag"], button [] [text "Volgende vraag"]]) 
             ])]])
-          , (60, [ button [] [text "Dat zoeken we op"]
-                 , img [src "images/astrid.jpg"] []])
+          , (70, rows
+                 [ (5, [], [button [] [text "Dat zoeken we op"]]) --TODO: belletje om te zoeken
+                --  , (95, [img [src "images/astrid.jpg", style "width" "100%"] []])
+                 , (95, [], [embed [type_ "text/html", src "https://nl.wikipedia.org", style "width" "100%", style "height" "100%"] []])
+                 ])
           ])
-        , (10, cols
+        , (10, [style "background-color" "white", style "opacity" "0.8"], cols
           [ (10, [clock millisdiff])
-          , (80, [letters model])
+          , (70, [letters model])
           , (10, [punten 500])
           ])
+        , (5, [], [])
         ])
 
 -- gegeven aantal milliseconden tot einde, maak klok
@@ -106,16 +110,17 @@ clock : Int -> Html Msg
 clock millisdiff = 
   let second = modBy 60 (millisdiff // 1000)
       minute = (millisdiff // 1000) // 60
-  in Svg.svg [Svg.width "100", Svg.height "50", Svg.viewBox "0 0 100 50"]
-             [ Svg.rect [Svg.x "0", Svg.y "0", Svg.width "75", Svg.height "50", Svg.fill "#ff0000"] []
-             , Svg.circle [Svg.cx "75", Svg.cy "25", Svg.r "25", Svg.fill "#ff0000"] []
-             , Svg.circle [Svg.cx "75", Svg.cy "25", Svg.r "21", Svg.stroke "#ffffff", Svg.strokeWidth "2", Svg.fill "none"] []
-             , Svg.foreignObject [Svg.x "55", Svg.y "15", Svg.width "50", Svg.height "50"] [div [style "color" "white"] [text (String.fromInt minute ++ ":" ++ String.fromInt second)]]]
+  in Svg.svg [Svg.width "100%", Svg.height "100%", Svg.viewBox "0 0 100% 100%"]
+             [ Svg.rect [Svg.x "0", Svg.y "0", Svg.width "calc(100% - 5cqh)", Svg.height "100%", Svg.fill "#ff0000"] []
+             , Svg.circle [Svg.cx "calc(100% - 5cqh)", Svg.cy "50%", Svg.r "5cqh", Svg.fill "#ff0000"] []
+             , Svg.circle [Svg.cx "calc(100% - 5cqh)", Svg.cy "50%", Svg.r "4.5cqh", Svg.stroke "#ffffff", Svg.strokeWidth "2", Svg.fill "none"] []
+             , Svg.foreignObject [Svg.x "calc(100% - 10cqh)", Svg.y "0", Svg.width "10cqh", Svg.height "100%"] [div ([style "color" "white", style "text-align" "center"]++centeringstuff) [text (String.fromInt minute ++ ":" ++ String.fromInt second)]]]
 
 letters : Model -> Html Msg
 letters model =
-  table [] [ tr [] (List.repeat 12 (th [style "border" "1px solid black"] [text "A"]))
-           , tr [style "color" "red"] (List.map (String.fromInt >> text >> \i -> td [] [i]) [1,2,3,4,5,6,7,8,9,10,11,12])]
+  table centeringstuff 
+        [ tr [] (List.repeat 12 (th [style "border" "1px solid black"] [text "A"]))
+        , tr [style "color" "red"] (List.map (String.fromInt >> text >> \i -> td [] [i]) [1,2,3,4,5,6,7,8,9,10,11,12])]
 
 punten : Int -> Html Msg
 punten x =
@@ -125,3 +130,6 @@ punten x =
           , Svg.circle [Svg.cx "25", Svg.cy "25", Svg.r "21", Svg.stroke "#ffffff", Svg.strokeWidth "2", Svg.fill "none"] []
           , Svg.foreignObject [Svg.x "10", Svg.y "15", Svg.width "50", Svg.height "50"] [div [style "color" "white"] [text (String.fromInt x)]]]
 
+
+centeringstuff : List (Attribute msg)
+centeringstuff = [style "top" "50%", style "left" "50%", style "position" "absolute", style "transform" "translate(-50%, -50%)"]
--- src/Utils.elm
+++ src/Utils.elm
@@ -2,8 +2,9 @@ module Utils exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-rows : List (Int, List (Html a)) -> List (Html a)
-rows xs = List.intersperse (div [style "clear" "both"] []) 
-                           (List.map (\(i,r) -> div [class "row", style "height" (String.fromInt i ++ "%")] r) xs)
+rows : List (Int, List (Attribute a), List (Html a)) -> List (Html a)
+rows xs = List.intersperse (div [style "clear" "both", style "display" "table"] []) 
+                           (List.map (\(i,s,r) -> div ([class "row", style "height" (String.fromInt i ++ "%"), style "width" "100%", style "display" "flex"]++s) r) xs)
 cols : List (Int, List (Html a)) -> List (Html a)
-cols xs = List.map (\(i,r) -> div [class "column", style "width" (String.fromInt i ++ "%"), style "float" "left"] r) xs
+cols xs = List.map (\(i,r) -> div [class "column", style "float" "left", style "flex" (String.fromInt i ++ "%"), style "height" "100%"] r) xs
+--  style "width" (String.fromInt i ++ "%"),
